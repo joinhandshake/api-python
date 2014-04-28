de0da5deacdc69533108884e64c07a4a83524bcb
Phil Middleton <pjmiddle@mtu.edu>
2014-04-27 22:33:15 -0400

    Added update, and updated Handshake definitions to take variables for user information

--- handshake.py
+++ handshake.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 #Global Variables
-#api_access_key = "Token token=\"fb0255bd9c1640914181e2a1aff1d42e"
+#Potentially api token here
 
 class Handshake(object):
 	#Variables
@@ -16,48 +16,64 @@ class Handshake(object):
 		Appends the function url to the base API url
 		'''
 		access_url = self.base_url + url
-		data = self.__data()
 		headers = {'Authorization': 'Token token="fb0255bd9c1640914181e2a1aff1d42e"', 'content-type': 'application/json'}
 
 		if(action == "create"):
 			r = requests.post(access_url, data=data, headers=headers)
 		elif(action == "delete"):
 			r = requests.delete(access_url, data=data, headers=headers)
+		elif(action == "update"):
+			r = requests.put(access_url, data=data, headers=headers)
 		return r
 
-	def __data(self):
+
+	def data(self, email_address, username, first_name, last_name):
 		'''
-		Stuff
+		Takes in the paramters that get passed to User classes
 		'''
 		return json.dumps({
 			'user': {
-				'email_address':'amsilski@mtu.edu', 
-				'username':'amsilski', 
-				'first_name':'Ann', 
-				'last_name':'Silski',
+				'email_address': email_address, 
+				'username': username, 
+				'first_name': first_name, 
+				'last_name': last_name,
 				}
 			})
 
 
-	def create(self):
+	def create_user(self, email, username, first_name, last_name):
 		'''
 		Creates a user
 		URL: users
 		'''
 		url = "users"
 		action = "create"
-		results = self.__communicate(url, {}, action)
+		data = self.data(email, username, first_name, last_name)
+		results = self.__communicate(url, data, action)
 
 		return results
 
-	def delete(self):
+
+	def delete_user(self):
 		'''
 		Deletes a specified user
 		URL: users/delete
 		'''
 		url = "users/delete"
 		action = "delete"
+		data = self.data(email, username, first_name, last_name)
 		results = self.__communicate(url, {}, action)
 		return results
 
+
+	def update_user(self):
+		'''
+		Updates a Specified user
+		URL: users/update
+		'''
+		url = "users/update"
+		action = "update"
+		data = self.data(email, username, first_name, last_name)
+		results = self.__communicate(url, {}, action)
+		return results
 
